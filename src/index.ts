2f54cf6528813ef6d2b5c6ab07c5d3446d79d522
TawsTm <phil.wandel@hotmail.de>
2021-12-19 16:44:03 +0100

    initial Commit

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(express.json({limit: '1mb'}));
+
+app.listen(3000, () => console.log('Dieser Server ist nicht für die Darstellung einer Website angedacht.'));
+
+// List of all ID's that take part at the installation at this point.
+let devices: string[] = [];
+let newestID: string = '1234';
+
+app.post('/api', (request: express.Request, response: express.Response) => {
+    const requestData: UserData = request.body;
+
+    // Decide if the request is an Update or a subscription cancel.
+    const update: string = requestData.update;
+    // Falls das Gerät noch nicht erfasst wurde, gib ihm eine ID, ansonsten sag ihm, das er schon erfasst wurde.
+    const playerID: string = requestData.playerID;
+    if (update === 'initialize') {
+        if (!playerID) {
+            response.json({
+                type: 'init',
+                newID: newestID
+            });
+            devices.push(newestID);
+            console.log('new Player' + playerID + 'joined');
+        } else {
+            response.json({
+                type: 'init',
+                newID: 'already initialized'
+            });
+            console.log('Already initialized Device tried to init!');
+        }
+    } else if (update === 'update') {
+        response.json({
+            type: 'update',
+            newID: ''
+        });
+        if (!devices.some(device => device === playerID)) {
+            devices.push(playerID);
+            console.log('Device was not in Serverlist, but already got an ID');
+        } else {
+            console.log('Device is init and can be updated');
+        }
+    } else if (update === 'cancel') {
+        console.log('Player ' + playerID + ' canceled');
+    } else {
+        console.log('The update request (' + update + ') is not possible');
+    }
+});
+
+app.get('/api', (request: express.Request, response: express.Response) => {
+    response.json({
+        status: 'Es sollte Post als Anfrage gestellt werden'
+    });
+    console.log(request.body);
+});
+
+interface UserData {
+    playerID: string;
+    update: string;
+    deviceList: DevicePackage;
+}
+
+interface DevicePackage {
+    canvasElement: HTMLElement;
+    chart: any;
+    device: any;
+    rssi: number[];
+    lifetime: number;
+    playerID: string;
+  }
